106c5da101f92de7b60b9a4424e0336dd8106f0e
Zacharie Montreuil <zmontreuil@rrc.ca>
2025-02-26 13:39:02 -0600

    Add initial logic for capturing userName input

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("script running");
+const userFormNode = document.querySelector("#user-form");
+
+userFormNode.addEventListener("submit", (eventObj) => {
+    // stop the default behaviour of the form submit event
+    // in this case, do not create a new HTTP request    
+    eventObj.preventDefault();
+
+    const validInputs = validateUserForm();
+
+    if(validInputs) {
+        userFormNode.submit();
+    }
+});
+
+function validateUserForm() {
+    let validInputs = true;
+
+    // capture the values of the inputs
+    const userNameInputNode = document.querySelector("#field_user-name");
+    const userNameInputValue = userNameInputNode.value;
+
+    return validInputs;
+}
