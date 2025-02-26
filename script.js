eea49df5609b85985b34e5e82105c2b3a4c442e4
Zacharie Montreuil <zmontreuil@rrc.ca>
2025-02-26 14:12:41 -0600

    Add basic validation for username

--- script.js
+++ script.js
@@ -5,19 +5,36 @@ userFormNode.addEventListener("submit", (eventObj) => {
     // in this case, do not create a new HTTP request    
     eventObj.preventDefault();
 
-    const validInputs = validateUserForm();
+    const inputsAreValid = validateUserForm();
 
-    if(validInputs) {
+    if(inputsAreValid) {
         userFormNode.submit();
+    } else {
+        console.warn("Invalid inputs");
     }
 });
 
 function validateUserForm() {
-    let validInputs = true;
+    let inputsAreValid = true;
 
     // capture the values of the inputs
+    // VALIDATE USERNAME
     const userNameInputNode = document.querySelector("#field_user-name");
     const userNameInputValue = userNameInputNode.value;
 
-    return validInputs;
+    // "Blacklist" method: prevent disallowed values from being added
+    // cf. "Whitelist" method: allowing specific patterns of values
+    const invalidChars = ["#", "$", "%", "^", "*", "&", " "];
+
+    if(userNameInputValue.trim().length < 4) {
+        inputsAreValid = false;
+    }
+
+    invalidChars.forEach(c => {
+        if(userNameInputValue.includes(c)) {
+            inputsAreValid = false;
+        }
+    });
+
+    return inputsAreValid;
 }
